f1bae87ac82e8564cac5cbe22804a8646a9fcd04
Bhrikuty Aggarwal <bhrikuty.aggarwal@gmail.com>
2019-12-13 19:22:39 +0530

    Update run.ps1

--- PrintNamefun/run.ps1
+++ PrintNamefun/run.ps1
@@ -1,27 +1,11 @@
-using namespace System.Net
-# Input bindings are passed in via param block
-param($Request, $TriggerMetadata)
-# Write to the Azure Functions log stream
-Write-Host "PowerShell HTTP trigger function processed a request"
-# Interact with query parameters or the body of the request
-$name = $Request.Query.Name
-if (-not $name)
- {
-$name = $Request.Body.Name
+# POST method: $req
+$requestBody = Get-Content $req -Raw | ConvertFrom-Json
+$name = $requestBody.name
+
+# GET method: each querystring parameter is its own variable
+if ($req_query_name) 
+{
+    $name = $req_query_name 
 }
-if ($name)
- {
-$status = [HttpStatusCode]::OK
-$body = "Hello $name"
-}
-else
- {
-$status = [HttpStatusCode]::BadRequest
-$body = "Please pass a name on the query string or in the request body"
-}
-# Associate values to output bindings by calling 'Push-OutputBinding'.
-Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-StatusCode = $status
-Body = $body
-}
-)
+
+Out-File -Encoding Ascii -FilePath $res -inputObject "Hello $name"
